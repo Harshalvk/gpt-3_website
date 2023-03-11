fb17988085ceff54ea87aad56bcbcc5e7069b358
Harshal-0816 <harshalvkhobragade@gmail.com>
2023-03-11 19:48:59 +0530

    small change in navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -15,7 +15,7 @@ const Navbar = () => {
           <div className='gpt3__navbar-links_container'>
             <p><a href='#home'>Home</a></p>
             <p><a href='#wgpt3'>What is GPT?</a></p>
-            <p><a href='#possibility'>Opne AI</a></p>
+            <p><a href='#possibility'>Open AI</a></p>
             <p><a href='#features'>Case Studies</a></p>
             <p><a href='#blog'>Library</a></p>
           </div>
